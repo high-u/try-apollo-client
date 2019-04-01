8ef42dae22a88c4311909a4905e8ee2db6cc8daf
high-u <zen.crazyd@gmail.com>
2019-04-02 02:41:02 +0900

    Add User.vue

---
+++ src/views/User.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>vue-apollo example: users</h1>
+    <div v-for="user in users" :key="user.id">
+      <div>{{ user.id }}, {{ user.name }}, {{ user.age }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag"
+
+const FeedQuery = gql`
+{
+  users {
+    ...namename
+  }
+}
+fragment namename on User {
+  name
+  id
+  age
+}
+`;
+export default {
+  name: "User",
+  data: () => ({
+    users: []
+  }),
+  apollo: {
+    // This `users` match the` users` of GraphQL `type Query {users: [User]}`.
+    users: FeedQuery,
+  }
+};
+</script>
